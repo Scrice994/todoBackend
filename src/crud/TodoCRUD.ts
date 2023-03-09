130282277dcc2b75e0e37aa884a49e497e80ed46
Scrice <davidemassimo94@gmail.com>
2023-03-09 15:45:24 +0100

    better error handling: added tests for TodoCrud when functions return unknown errors

--- src/crud/TodoCRUD.ts
+++ src/crud/TodoCRUD.ts
@@ -1,111 +1,110 @@
-import { IRepository } from "src/repositories/IRepository";
-import { ICRUD } from "./ICRUD";
-import { TodoEntity } from "src/entities/TodoEntity";
-import { IEntity } from "src/entities/IEntity";
-import { DataStorageId } from "src/dataStorages/IDataStorage";
-import { ICRUDResponse } from "./ICRUD";
+import { IRepository } from 'src/repositories/IRepository';
+import { ICRUD } from './ICRUD';
+import { TodoEntity } from 'src/entities/TodoEntity';
+import { IEntity } from 'src/entities/IEntity';
+import { DataStorageId } from 'src/dataStorages/IDataStorage';
+import { ICRUDResponse } from './ICRUD';
 
 export class TodoCRUD implements ICRUD<TodoEntity> {
-  constructor(private repository: IRepository<TodoEntity>) {}
+    constructor(private repository: IRepository<TodoEntity>) {}
 
-  async read(): Promise<ICRUDResponse<TodoEntity[]>> {
+    async read(): Promise<ICRUDResponse<TodoEntity[]>> {
+        try {
+            const todos = await this.repository.getAll();
 
-    try {
-      const todos = await this.repository.getAll();
-
-      return {
-        statusCode: 200,
-        data: {
-          response: todos,
-        },
-      };
-
-    } catch (error) {
-      return this.unknownErrorResponse()
+            return {
+                statusCode: 200,
+                data: {
+                    response: todos,
+                },
+            };
+        } catch (error) {
+            return this.unknownErrorResponse();
+        }
     }
-  }
 
-  async create(newTodo: Omit<TodoEntity, "id">): Promise<ICRUDResponse<TodoEntity>> {
+    async create(
+        newTodo: Omit<TodoEntity, 'id'>
+    ): Promise<ICRUDResponse<TodoEntity>> {
+        try {
+            if (!newTodo.text) {
+                return {
+                    statusCode: 400,
+                    data: {
+                        message: 'Missing required @parameter text',
+                    },
+                };
+            }
 
-    try {
-      if (!newTodo.text) {
+            const result = await this.repository.insertOne(newTodo);
+
+            return {
+                statusCode: 200,
+                data: {
+                    response: result,
+                },
+            };
+        } catch (error) {
+            return this.unknownErrorResponse();
+        }
+    }
+
+    async update(
+        updateTodo: Required<IEntity> & Partial<TodoEntity>
+    ): Promise<ICRUDResponse<TodoEntity>> {
+        try {
+            if (!updateTodo.id) {
+                return {
+                    statusCode: 404,
+                    data: {
+                        message: 'Missing or invalid required @parameter id',
+                    },
+                };
+            }
+
+            const result = await this.repository.updateOne(updateTodo);
+
+            return {
+                statusCode: 200,
+                data: {
+                    response: result,
+                },
+            };
+        } catch (error) {
+            return this.unknownErrorResponse();
+        }
+    }
+
+    async delete(id: DataStorageId): Promise<ICRUDResponse<TodoEntity>> {
+        try {
+            if (!id) {
+                return {
+                    statusCode: 404,
+                    data: {
+                        message: 'Missing or invalid required @parameter id',
+                    },
+                };
+            }
+
+            const result = await this.repository.deleteOne(id);
+
+            return {
+                statusCode: 200,
+                data: {
+                    response: result,
+                },
+            };
+        } catch (error) {
+            return this.unknownErrorResponse();
+        }
+    }
+
+    private unknownErrorResponse() {
         return {
-          statusCode: 400,
-          data: {
-            message: "Missing required @parameter text",
-          },
+            statusCode: 500,
+            data: {
+                message: 'Unknown Error',
+            },
         };
-      }
-  
-      const result = await this.repository.insertOne(newTodo);
-  
-      return {
-        statusCode: 200,
-        data: {
-          response: result,
-        },
-      };
-    } catch (error) {
-      return this.unknownErrorResponse()
     }
-  }
-
-  async update(updateTodo: Required<IEntity> & Partial<TodoEntity>): Promise<ICRUDResponse<TodoEntity>> {
-
-    try {
-      if (!updateTodo.id) {
-        return {
-          statusCode: 404,
-          data: {
-            message: "Missing or invalid required @parameter id",
-          },
-        };
-      }
-  
-      const result = await this.repository.updateOne(updateTodo);
-  
-      return {
-        statusCode: 200,
-        data: {
-          response: result,
-        },
-      };
-    } catch (error) {
-      return this.unknownErrorResponse()
-    }
-  }
-
-  async delete(id: DataStorageId): Promise<ICRUDResponse<TodoEntity>> {
-
-    try {
-      if (!id) {
-        return {
-          statusCode: 404,
-          data: {
-            message: "Missing or invalid required @parameter id",
-          },
-        };
-      }
-  
-      const result = await this.repository.deleteOne(id);
-  
-      return {
-        statusCode: 200,
-        data: {
-          response: result,
-        },
-      }; 
-    } catch (error) {
-      return this.unknownErrorResponse()
-    }
-  }
-
-  unknownErrorResponse(){
-    return {
-      statusCode: 500,
-      data: {
-        message: "Unknown Error"
-      }
-    }
-  }
 }
--- test/unit/crud/TodoCRUD.test.ts
+++ test/unit/crud/TodoCRUD.test.ts
@@ -1,108 +1,178 @@
-import { describe, it, expect } from "@jest/globals";
-import { RepositoryMock } from "../__mocks__/Repository.mock";
-import { TodoEntity } from "../../../src/entities/TodoEntity";
-import { TodoCRUD } from "../../../src/crud/TodoCRUD";
+import { describe, it, expect } from '@jest/globals';
+import { RepositoryMock } from '../__mocks__/Repository.mock';
+import { TodoEntity } from '../../../src/entities/TodoEntity';
+import { TodoCRUD } from '../../../src/crud/TodoCRUD';
 
-describe("unit", () => {
-    describe("crud", () => {
-        describe("TodoCRUD", () => {
+describe('unit', () => {
+    describe('crud', () => {
+        describe('TodoCRUD', () => {
             const TodoRepositoryMock = new RepositoryMock<TodoEntity>();
             const CRUD = new TodoCRUD(TodoRepositoryMock);
             const fakeResponse = {
                 id: 'mockId',
                 text: 'mockText',
-                completed: false
-            }
+                completed: false,
+            };
             const fakeResponseUpdate = {
-                    id: 'mockId',
-                    text: 'newText',
-                    completed: true
+                id: 'mockId',
+                text: 'newText',
+                completed: true,
+            };
+            const unknownErrorResponse = {
+                statusCode: 500,
+                data: {
+                    message: 'Unknown Error',
+                },    
             }
 
-            describe("read()", () => {
-                it("Should return all elements from the repository", async () => {
-                    TodoRepositoryMock.getAll.mockImplementationOnce(() => Promise.resolve([fakeResponse]))
-                
+            describe('read()', () => {
+                it('Should return all elements from the repository', async () => {
+                    TodoRepositoryMock.getAll.mockImplementationOnce(() =>
+                        Promise.resolve([fakeResponse])
+                    );
+
                     expect(await CRUD.read()).toEqual({
                         statusCode: 200,
                         data: {
-                            response: [fakeResponse]
-                        }
-                    })
-                })
+                            response: [fakeResponse],
+                        },
+                    });
+                });
+
+                it('Should return error message when unknown error occurs', async () => {
+                    TodoRepositoryMock.getAll.mockImplementationOnce(() => {
+                        throw new Error();
+                    });
+
+                    expect(await CRUD.read()).toEqual(unknownErrorResponse)
+                });
             });
 
-            describe("create()", () => {
-                it("Should create a new element in the repository",async () => {
-                    TodoRepositoryMock.insertOne.mockImplementationOnce(() => Promise.resolve(fakeResponse))
+            describe('create()', () => {
+                it('Should create a new element in the repository', async () => {
+                    TodoRepositoryMock.insertOne.mockImplementationOnce(() =>
+                        Promise.resolve(fakeResponse)
+                    );
 
-                    expect(await CRUD.create({text: "mockText"})).toEqual({
+                    expect(await CRUD.create({ text: 'mockText' })).toEqual({
                         statusCode: 200,
                         data: {
-                            response: fakeResponse
-                        }
-                    })
-                })
+                            response: fakeResponse,
+                        },
+                    });
+                });
 
-                it("Should throw and Error when try to create a TodoEntity object with a empty text", async () => {
-                    const response = await CRUD.create(JSON.parse(JSON.stringify({})))
+                it('Should throw and Error when try to create a TodoEntity object with a empty text', async () => {
+                    const response = await CRUD.create(
+                        JSON.parse(JSON.stringify({}))
+                    );
 
                     expect(response).toEqual({
                         statusCode: 400,
                         data: {
-                            message: "Missing required @parameter text"
-                        }
-                    })
-                  })
-            })
+                            message: 'Missing required @parameter text',
+                        },
+                    });
+                });
 
-            describe("update()", () => {
-                it("should return the updated element from the repository", async () => {
-                    TodoRepositoryMock.updateOne.mockImplementationOnce(() => Promise.resolve(fakeResponseUpdate))
+                it('Should return error message when unknown error occurs', async () => {
+                    TodoRepositoryMock.insertOne.mockImplementationOnce(() => {
+                        throw new Error();
+                    });
 
-                    expect(await CRUD.update({id: 'mockId', completed: true, text: 'newText'})).toEqual({
+                    expect(await CRUD.create({ text: 'mockText' })).toEqual(unknownErrorResponse)
+                });
+            });
+
+            describe('update()', () => {
+                it('should return the updated element from the repository', async () => {
+                    TodoRepositoryMock.updateOne.mockImplementationOnce(() =>
+                        Promise.resolve(fakeResponseUpdate)
+                    );
+
+                    expect(
+                        await CRUD.update({
+                            id: 'mockId',
+                            completed: true,
+                            text: 'newText',
+                        })
+                    ).toEqual({
                         statusCode: 200,
                         data: {
-                            response: fakeResponseUpdate
-                        }
-                    })
-                })
+                            response: fakeResponseUpdate,
+                        },
+                    });
+                });
 
-                it("should return error if id is invalid or not found", async () => {
-                    const response = await CRUD.update(JSON.parse(JSON.stringify({text: "sampleText", completed: true})))
-                    
+                it('should return error if id is invalid or not found', async () => {
+                    const response = await CRUD.update(
+                        JSON.parse(
+                            JSON.stringify({
+                                text: 'sampleText',
+                                completed: true,
+                            })
+                        )
+                    );
+
                     expect(response).toEqual({
                         statusCode: 404,
                         data: {
-                            message: "Missing or invalid required @parameter id"
-                        }
-                    })
-                })
-            })
+                            message:
+                                'Missing or invalid required @parameter id',
+                        },
+                    });
+                });
 
-            describe("delete()", () => {
-                it("should return the deleted element from the repository", async () => {
-                    TodoRepositoryMock.deleteOne.mockImplementationOnce(() => Promise.resolve(fakeResponse))
+                it('Should return error message when unknown error occurs', async () => {
+                    TodoRepositoryMock.updateOne.mockImplementationOnce(() => {
+                        throw new Error();
+                    });
+
+                    expect(await CRUD.update({
+                        id: 'mockId',
+                        completed: true,
+                        text: 'newText',
+                    })).toEqual(unknownErrorResponse)
+                });
+            });
+
+            describe('delete()', () => {
+                it('should return the deleted element from the repository', async () => {
+                    TodoRepositoryMock.deleteOne.mockImplementationOnce(() =>
+                        Promise.resolve(fakeResponse)
+                    );
 
                     expect(await CRUD.delete('mockId')).toEqual({
                         statusCode: 200,
                         data: {
-                            response: fakeResponse
-                        }
-                    })
-                })
+                            response: fakeResponse,
+                        },
+                    });
+                });
 
-                it("should return error if id is invalid or not found", async () => {
-                    const response = await CRUD.delete(JSON.parse(JSON.stringify('')))
+                it('should return error if id is invalid or not found', async () => {
+                    const response = await CRUD.delete(
+                        JSON.parse(JSON.stringify(''))
+                    );
 
                     expect(response).toEqual({
                         statusCode: 404,
                         data: {
-                            message: "Missing or invalid required @parameter id"
-                        }
-                    })
-                })
-            })
+                            message:
+                                'Missing or invalid required @parameter id',
+                        },
+                    });
+                });
+
+                it('Should return error message when unknown error occurs', async () => {
+                    TodoRepositoryMock.deleteOne.mockImplementationOnce(() => {
+                        throw new Error();
+                    });
+
+                    expect(await CRUD.delete('mockId')).toEqual(unknownErrorResponse)
+                });
+
+            });
         });
     });
 });
