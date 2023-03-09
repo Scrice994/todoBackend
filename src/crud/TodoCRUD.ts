058a94c84c710cd435f7146b6f8472cef3f349ac
Scrice <davidemassimo94@gmail.com>
2023-03-09 15:22:35 +0100

    added better error handling in TodoCRUD

--- src/crud/TodoCRUD.ts
+++ src/crud/TodoCRUD.ts
@@ -9,77 +9,103 @@ export class TodoCRUD implements ICRUD<TodoEntity> {
   constructor(private repository: IRepository<TodoEntity>) {}
 
   async read(): Promise<ICRUDResponse<TodoEntity[]>> {
-    const todos = await this.repository.getAll();
 
-    return {
-      statusCode: 200,
-      data: {
-        response: todos,
-      },
-    };
-  }
+    try {
+      const todos = await this.repository.getAll();
 
-  async create(
-    newTodo: Omit<TodoEntity, "id">
-  ): Promise<ICRUDResponse<TodoEntity>> {
-    if (!newTodo.text) {
       return {
-        statusCode: 400,
+        statusCode: 200,
         data: {
-          message: "Missing required @parameter text",
+          response: todos,
         },
       };
+
+    } catch (error) {
+      return this.unknownErrorResponse()
     }
-
-    const result = await this.repository.insertOne(newTodo);
-
-    return {
-      statusCode: 200,
-      data: {
-        response: result,
-      },
-    };
   }
 
-  async update(
-    updateTodo: Required<IEntity> & Partial<TodoEntity>
-  ): Promise<ICRUDResponse<TodoEntity>> {
-    if (!updateTodo.id) {
+  async create(newTodo: Omit<TodoEntity, "id">): Promise<ICRUDResponse<TodoEntity>> {
+
+    try {
+      if (!newTodo.text) {
+        return {
+          statusCode: 400,
+          data: {
+            message: "Missing required @parameter text",
+          },
+        };
+      }
+  
+      const result = await this.repository.insertOne(newTodo);
+  
       return {
-        statusCode: 404,
+        statusCode: 200,
         data: {
-          message: "Missing or invalid required @parameter id",
+          response: result,
         },
       };
+    } catch (error) {
+      return this.unknownErrorResponse()
     }
+  }
 
-    const result = await this.repository.updateOne(updateTodo);
+  async update(updateTodo: Required<IEntity> & Partial<TodoEntity>): Promise<ICRUDResponse<TodoEntity>> {
 
-    return {
-      statusCode: 200,
-      data: {
-        response: result,
-      },
-    };
+    try {
+      if (!updateTodo.id) {
+        return {
+          statusCode: 404,
+          data: {
+            message: "Missing or invalid required @parameter id",
+          },
+        };
+      }
+  
+      const result = await this.repository.updateOne(updateTodo);
+  
+      return {
+        statusCode: 200,
+        data: {
+          response: result,
+        },
+      };
+    } catch (error) {
+      return this.unknownErrorResponse()
+    }
   }
 
   async delete(id: DataStorageId): Promise<ICRUDResponse<TodoEntity>> {
-    if (!id) {
+
+    try {
+      if (!id) {
+        return {
+          statusCode: 404,
+          data: {
+            message: "Missing or invalid required @parameter id",
+          },
+        };
+      }
+  
+      const result = await this.repository.deleteOne(id);
+  
       return {
-        statusCode: 404,
+        statusCode: 200,
         data: {
-          message: "Missing or invalid required @parameter id",
+          response: result,
         },
-      };
+      }; 
+    } catch (error) {
+      return this.unknownErrorResponse()
     }
+  }
 
-    const result = await this.repository.deleteOne(id);
-
+  unknownErrorResponse(){
     return {
-      statusCode: 200,
+      statusCode: 500,
       data: {
-        response: result,
-      },
-    };
+        message: "Unknown Error"
+      }
+    }
   }
 }
